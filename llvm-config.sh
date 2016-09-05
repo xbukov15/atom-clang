99d64ac2a3f2f56446df0064b6bc3d3f63e89ecb
Joe Roback <joe.roback@gmail.com>
2016-09-05 09:45:41 -0600

    move llvm-config to the top search list

--- llvm-config.sh
+++ llvm-config.sh
@@ -4,7 +4,8 @@
 # known naming conventions of llvm-config, looking for the latest version
 
 [[ -z ${LLVM_CONFIG} ]] \
-    && LLVM_CONFIG=$(which llvm-config-4.2 2>/dev/null) \
+    && LLVM_CONFIG=$(which llvm-config 2>/dev/null) \
+    || LLVM_CONFIG=$(which llvm-config-4.2 2>/dev/null) \
     || LLVM_CONFIG=$(which llvm-config-4.1 2>/dev/null) \
     || LLVM_CONFIG=$(which llvm-config-4.0 2>/dev/null) \
     || LLVM_CONFIG=$(which llvm-config-3.9 2>/dev/null) \
@@ -13,7 +14,6 @@
     || LLVM_CONFIG=$(which llvm-config-mp-4.1 2>/dev/null) \
     || LLVM_CONFIG=$(which llvm-config-mp-4.0 2>/dev/null) \
     || LLVM_CONFIG=$(which llvm-config-mp-3.9 2>/dev/null) \
-    || LLVM_CONFIG=$(which llvm-config-mp-3.8 2>/dev/null) \
-    || LLVM_CONFIG=$(which llvm-config 2>/dev/null)
+    || LLVM_CONFIG=$(which llvm-config-mp-3.8 2>/dev/null)
 
 echo -n ${LLVM_CONFIG}
