db79ccef8f4415c794bd57525ac3ec1b3228fa2d
Joe Roback <joe.roback@gmail.com>
2016-12-22 19:52:22 -0700

    move compiler define workarounds into CMakeLists.txt

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-cmake-js compile --std gnu++11 --runtime=${npm_config_runtime} --runtime-version=${npm_config_target} --arch=${npm_config_arch}
+
+cmake-js \
+    compile \
+    --prefer-make \
+    --std 'gnu++11' \
+    --runtime=${npm_config_runtime} \
+    --runtime-version=${npm_config_target} \
+    --arch=${npm_config_arch}
